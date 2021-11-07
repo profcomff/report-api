a93e61bb7b1a1150825e7bee93e66048655cef5f
Roman Dyakov <dyakovri@gmail.com>
2021-11-07 19:07:29 +0100

    Import fix

--- report_api/settings.py
+++ report_api/settings.py
@@ -1,17 +1,18 @@
 import string
+from typing import Optional
 from functools import lru_cache
 
 from pydantic import BaseSettings
-from pydantic.networks import AnyHttpUrl, PostgresDsn
+from pydantic.networks import HttpUrl, PostgresDsn
 
 
 class Settings(BaseSettings):
     ROOT: Optional[HttpUrl] = None
     DB_DSN: PostgresDsn = None
 
-    EMAIL_CONFIRM_SUCCSESS: AnyHttpUrl = 'https://report.profcomff.com/email_confirmation_success'
-    EMAIL_CONFIRM_RETRY: AnyHttpUrl = 'https://report.profcomff.com/email_confirmation_retry'
-    EMAIL_CONFIRM_ERROR: AnyHttpUrl = 'https://report.profcomff.com/email_confirmation_error'
+    EMAIL_CONFIRM_SUCCSESS: HttpUrl = 'https://report.profcomff.com/email_confirmation_success'
+    EMAIL_CONFIRM_RETRY: HttpUrl = 'https://report.profcomff.com/email_confirmation_retry'
+    EMAIL_CONFIRM_ERROR: HttpUrl = 'https://report.profcomff.com/email_confirmation_error'
 
     PIN_SYMBOLS: str = string.ascii_uppercase + string.digits
     PIN_LENGTH: int = 6
